b0cf71b3337569035018130e9c1e40a6b0339217
Y23IT022 <bssindhu006@gmail.com>
2025-06-15 22:17:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chat from './Chat';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CodTech Real-Time Chat</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
